9e9d1b2bdb3eba777229577db15265f759e5839a
DELL <psbgoudar777@gmail.com>
2024-09-28 17:29:55 +0530

    add Lab001.

--- Ex_03062024/multiplication.py
+++ Ex_03062024/multiplication.py
@@ -1,1 +1,10 @@
-print(3*3)
+import time
+
+from selenium import webdriver
+def test_practice():
+    driver=webdriver.chrome()
+    driver.get("https://awesomeqa.com/practice.html")
+
+    time.sleep(5)
+    driver.quit()
+
